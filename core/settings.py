d4d9f5cc77cb14b4e6122a17af2b811947d27c67
HypeStalker <hypestalkerv1@gmail.com>
2023-06-08 12:16:48 +0200

    dbqlite

--- core/settings.py
+++ core/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 

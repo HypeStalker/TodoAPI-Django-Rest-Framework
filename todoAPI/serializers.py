58c058f931e6ad2f08de148cb8e4f68746c6787e
HypeStalker <hypestalkerv1@gmail.com>
2023-06-08 23:16:44 +0200

    removed author url

--- todoAPI/serializers.py
+++ todoAPI/serializers.py
@@ -8,14 +8,12 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source="author.username")
     detail_url = serializers.HyperlinkedIdentityField(view_name="todo_detail", lookup_field="pk")
-    author_url = serializers.HyperlinkedIdentityField(view_name="user_detail", lookup_field="pk")
     class Meta:
         model = Task
         fields = [
             "detail_url",
             "id",
             "author",
-            "author_url",
             "body",
             "isCompleted",
             "created_at",
